76484f94cee7561f59d28252a5fb87ad15581d65
Alexander Kilian <alexanderkilian@gmx.de>
2022-01-20 09:24:05 +0100

    no message

---
+++ src/io/openems/controller/emsig/ojalgo/PV.java
@@ -0,0 +1,20 @@
+package io.openems.controller.emsig.ojalgo;
+
+// import io.openems.controller.emsig.ojalgo.Photovoltaic.PV;
+
+// import org.ojalgo.optimisation.Variable;
+
+// is defining the static class redundant?
+
+public class PV {
+	
+		public static class Power{
+			public int prod;
+		}
+		
+	
+		public final Power power = new Power();
+
+		public PV() {
+	}
+}
